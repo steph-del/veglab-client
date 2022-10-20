f9decf794af5df6339b6177a8f5af7376b2a2842
Stéphane <stephanedelplanque@yahoo.fr>
2022-10-20 22:08:52 +0200

    [up] env. var according to latest stack configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,13 +4,13 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'http://${HOST}',
-  apiBaseUrl: 'http://${HOST}:${API_PORT}/api',
+  baseUrl: 'https://${CLIENT_DEV_HOST}',
+  apiBaseUrl: 'https://${API_HOST}/api',
   esAuthorizationPassword: '${ELASTIC_PASSWORD}',
-  esBaseUrl: 'http://${HOST}:${ES_PORT}',
+  esBaseUrl: 'https://${ES_HOST}',
   esRepoAuthorizationPassword: '${ES_REPO_PASSWORD}',
   esBaseflorTraitsApi: 'http://51.38.37.216:9200/baseflor',
-  pdfBaseUrl: 'http://${HOST}:${API_PORT}/media/veglab/pdf/',
+  pdfBaseUrl: 'https://${API_HOST}/media/veglab/pdf/',
   app : {
     title:           'VegLab',
     unsetTokenValue: 'unset',
@@ -18,11 +18,11 @@ export const environment = {
   },
   sso: {
     clientId:         '${SSO_CLIENT_ID}',
-    baseUrl:          'http://${SSO_HOST}:${SSO_PORT}${SSO_URI}',
-    loginEndpoint:    'http://${SSO_HOST}:${SSO_PORT}${SSO_URI}${SSO_LOGIN_ENDPOINT}',
-    logoutEndpoint:   'http://${SSO_HOST}:${SSO_PORT}${SSO_URI}${SSO_LOGOUT_ENDPOINT}',
-    identiteEndpoint: 'http://${SSO_HOST}:${SSO_PORT}${SSO_URI}${SSO_REFRESH_ENDPOINT}',
-    refreshEndpoint:  'http://${SSO_HOST}:${SSO_PORT}${SSO_URI}${SSO_REFRESH_ENDPOINT}',
+    baseUrl:          'https://${SSO_HOST}${SSO_URI}',
+    loginEndpoint:    'https://${SSO_HOST}${SSO_URI}${SSO_LOGIN_ENDPOINT}',
+    logoutEndpoint:   'https://${SSO_HOST}${SSO_URI}${SSO_LOGOUT_ENDPOINT}',
+    identiteEndpoint: 'https://${SSO_HOST}${SSO_URI}${SSO_REFRESH_ENDPOINT}',
+    refreshEndpoint:  'https://${SSO_HOST}${SSO_URI}${SSO_REFRESH_ENDPOINT}',
     refreshInterval:  '${SSO_REFRESH_INTERVAL}',
     roles: {
       admin: 'admin'
