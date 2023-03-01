a0c1c06aef4bf84bc0036ff2800517029c463572
Stéphane <stephanedelplanque@yahoo.fr>
2023-03-01 09:14:46 +0100

    [update] the new API auth needs the 'Bearer' keyword in the auth. header

--- src/app/_interceptors/auth.interceptor.ts
+++ src/app/_interceptors/auth.interceptor.ts
@@ -24,7 +24,7 @@ export class AuthInterceptor implements HttpInterceptor {
       if (token !== null) {
         request = request.clone({
           setHeaders: {
-              Authorization: `${token}`
+              Authorization: `Bearer ${token}`
           }
         });
       }
